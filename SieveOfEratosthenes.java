eacd63bf9240228f4c311d27feb34723b2a93708
Mamun-Al-Babu-Shikder <49284765+Mamun-Al-Babu-Shikder@users.noreply.github.com>
2022-02-13 19:28:32 +0600

    Sieve of Eratosthenes
    The sieve of Eratosthenes algorithm generates all the primes up to a given limit. This is a common and fast algorithm used to generate a list of primes up to a given limit. It works by making a list from 1 to N, and then iterating through the list and progressively removing non-prime, composite numbers until only primes are left in a list.
    
    Example
    
    For example, if we wanted to generate all the primes up to the number 30, we first create a list of numbers from 1 to 30 and follow the numbered steps:
    
    1  2  3  4  5  6  7  8  9  10 
    11 12 13 14 15 16 17 18 19 20 
    21 22 23 24 25 26 27 28 29 30
    
    (Step 1) The algorithm starts at the first number, 1, and removes it because it is not a prime number.
    (Step 2) The next number is 2, which is a prime so it stays, but now all multiples of 2 are removed from the list: 4, 6, 8, 10, etc.
    
       2  3     5     7     9    
    11    13    15    17    19    
    21    23    25    27    29
    
    (Step 3) The next number is 3, which is a prime so it stays, but now all multiples of 3 are removed from the list: 6, 9, etc.
    
       2  3     5     7       
    11    13          17    19  
          23    25          29
     
    (Step 4) The next number is 5, which is a prime so it stays, but now all multiples of 5 are removed from the list: 10, 15, etc.
    
       2  3     5     7       
    11    13          17    19  
          23                29
     
    (Step 5) All the numbers now, 7, 11, 13, etc., are all primes and there are no more multiples of numbers we can remove from the list, so we are done and the list that is left is our list of primes.

---
+++ SieveOfEratosthenes.java
@@ -0,0 +1,37 @@
+/**
+ * Topic : Number Theory
+ * Name : Sieve of Eratosthenes
+ * Objective : Find out the prime numbers from 1 to n
+ */
+
+package algorithm;
+
+import java.util.Scanner;
+
+public class SieveOfEratosthenes {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            printPrimes(n);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printPrimes(int n) {
+        int[] primes = new int[n + 1];
+        int m = (int) Math.sqrt(n);
+        for (int i = 2; i < m; i++) {
+            if (primes[i] == 0)
+                for (int j = i * i; j <= n; j += i)
+                    primes[j] = 1;
+
+        }
+        for (int i = 2; i <= n; i++) {
+            if (primes[i] == 0)
+                System.out.println(i);
+        }
+    }
+
+}
